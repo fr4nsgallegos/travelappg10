dee59650198e6cf90d0c4c644eafe91d8cb5a17b
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-11-24 11:12:03 -0500

    shared preferences page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:travelappg10/pages/home_page.dart';
+import 'package:travelappg10/pages/shared_preferences_page.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: HomePage(),
+      home: SharedPreferencesPage(),
       debugShowCheckedModeBanner: false,
     ),
   );
--- lib/pages/shared_preferences_page.dart
+++ lib/pages/shared_preferences_page.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class HomePage extends StatefulWidget {
+class SharedPreferencesPage extends StatefulWidget {
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<SharedPreferencesPage> createState() => _SharedPreferencesPageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _SharedPreferencesPageState extends State<SharedPreferencesPage> {
   int contador = 0;
+  String user = "";
 
   Future<void> _saveContador() async {
     SharedPreferences _prefs = await SharedPreferences.getInstance();
     //GUARDAR EL CONTADOR
     _prefs.setInt('counter', contador);
     //GUARDAR MÁS INFORMACIÓN
-    _prefs.setString('username', 'Jhonny"');
+    _prefs.setString('username', 'Jhonny');
     _prefs.setDouble('heigth', 1.75);
     _prefs.setBool('isLoggetIn', true);
     _prefs.setStringList('favorites', ['Flutter', 'DART', 'Developer']);
@@ -24,7 +25,18 @@ class _HomePageState extends State<HomePage> {
     SharedPreferences _prefs = await SharedPreferences.getInstance();
     //LEER EL CONTADOR
     contador = _prefs.getInt('counter') ?? 0;
+    user = _prefs.getString('username') ?? "-";
     setState(() {});
+  }
+
+  Future<void> clearContador() async {
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+
+    //Eliminar un dato en específico
+    _prefs.remove('counter');
+
+    //eliminar todos los datos
+    // _prefs.clear();
   }
 
   @override
@@ -48,9 +60,21 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
+              user,
+              style: TextStyle(fontSize: 50),
+            ),
+            Text(
               contador.toString(),
               style: TextStyle(fontSize: 50),
-            )
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  clearContador();
+                },
+                child: Text("Eliminar shared preferences counter"))
           ],
         ),
       ),
